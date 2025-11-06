a4f7174025b43969195a9b474cb367646b6eeec5
Alex <learnhtml1921@gmail.com>
2025-11-06 08:08:37 +0530

    Add assembly code for summing an array of numbers

---
+++ lab2.asm
@@ -0,0 +1,22 @@
+DATA SEGMENT
+ A DB 1,2,3,4,5,6,7,8,9,10
+DATA ENDS
+CODE SEGMENT
+ ASSUME CS:CODE, DS:DATA
+ START:
+ MOV AX, DATA
+ MOV DS, AX
+ MOV CL, 10
+ LEA SI, A ;use SI for storing the data
+ MOV AH, 100
+ MOV AL, 00
+ L1:
+ ADD AL, [SI] ;use square brackets to dereference SI
+ INC SI
+ DEC CL
+ CMP CL, 00
+ JNZ L1
+ MOV AH, 4CH
+ INT 21H
+ CODE ENDS
+END START
